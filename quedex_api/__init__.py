90388ab5307353202579e92eda18828f40da9f1f
Wiktor Gromniak <wgromniak@quedex.net>
2017-07-19 16:54:29 +0000

    Added user stream to quedex_api __init__.

--- quedex_api/__init__.py
+++ quedex_api/__init__.py
@@ -2,4 +2,5 @@ from exchange import Exchange
 from market_stream import MarketStream, MarketStreamListener
 from market_stream_client import MarketStreamClientFactory
 from trader import Trader
-
+from user_stream import UserStream, UserStreamListener
+from user_stream_client import UserStreamClientFactory
