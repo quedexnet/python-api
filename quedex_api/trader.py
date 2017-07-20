80afe25d245bd126e931cdfa4f52937ee2baade1
Wiktor Gromniak <wgromniak@quedex.net>
2017-07-20 08:26:53 +0000

    Removed logic from constructor in Trader.

--- quedex_api/trader.py
+++ quedex_api/trader.py
@@ -4,13 +4,26 @@ import pgpy
 
 
 class Trader(object):
-  def __init__(self, account_id, private_key):
+  def __init__(self, account_id, private_key_str):
     self.account_id = account_id
-    self.private_key = pgpy.PGPKey()
-    self.private_key.parse(private_key)
+    self._private_key_str = private_key_str
+    self._private_key = None
 
   def decrypt_private_key(self, passphrase):
+    if not self._private_key:
+      self._parse_key()
+
     # this is what happens internally in PGPKey.unlock, but we want to leave the key decrypted not
     # only within a context manager as PGPKey.unlock does
-    for subkey in itertools.chain([self.private_key], self.private_key.subkeys.values()):
+    for subkey in itertools.chain([self._private_key], self._private_key.subkeys.values()):
       subkey._key.unprotect(passphrase)
+
+  @property
+  def private_key(self):
+    if not self._private_key:
+      self._parse_key()
+    return self._private_key
+
+  def _parse_key(self):
+    self._private_key = pgpy.PGPKey()
+    self._private_key.parse(self._private_key_str)
