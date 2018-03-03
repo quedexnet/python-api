e24b1f969b823cd29c1d16896beb78c2e1e5dd16
Wiktor Gromniak <wgromniak@quedex.net>
2018-03-03 14:57:57 +0000

    #16 Fixed imports in main __init__ for Python 3 compatibility.

--- quedex_api/__init__.py
+++ quedex_api/__init__.py
@@ -1,7 +1,7 @@
-from exchange import Exchange
-from market_stream import MarketStream, MarketStreamListener
-from market_stream_client import MarketStreamClientFactory
-from trader import Trader
-from user_stream import UserStream, UserStreamListener
-from user_stream_client import UserStreamClientFactory
-from keys import quedex_public_key
+from .exchange import Exchange
+from .market_stream import MarketStream, MarketStreamListener
+from .market_stream_client import MarketStreamClientFactory
+from .trader import Trader
+from .user_stream import UserStream, UserStreamListener
+from .user_stream_client import UserStreamClientFactory
+from .keys import quedex_public_key
