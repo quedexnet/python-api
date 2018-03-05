18f4a2b0a6741a01b525fcbc1469994a744d0cc2
Maciej Chałapuk <m.chalapuk@quedex.net>
2018-03-05 20:55:47 +0100

    Extracted e2e_test.sh that does actual testing from run scripts.

--- run_end_to_end_tests_python3.sh
+++ run_end_to_end_tests_python3.sh
@@ -7,25 +7,5 @@ fi
 . venv3/bin/activate
 pip install -r requirements
 
-sed -i -e 's/wss:\/\/api.quedex.net/ws:\/\/localhost:8080/g' examples/simple_trading.py
+./e2e_test.sh
 
-python -m unittest discover end_to_end_tests &
-TESTS_PID=$!
-
-while ! nc -z localhost 8080; do
-  sleep 0.2
-done
-
-PYTHONPATH=".:.." python examples/simple_trading.py &
-CLIENT_PID=$!
-
-wait $TESTS_PID
-TESTS_RESULT=$?
-
-kill $CLIENT_PID
-
-sed -i -e 's/ws:\/\/localhost:8080/wss:\/\/api.quedex.net/g' examples/simple_trading.py
-
-exit $TESTS_RESULT
-
-deactivate
