04b019d94ec749598d863b9840093818225ded79
Maciej Chałapuk <m.chalapuk@quedex.net>
2018-03-05 21:00:05 +0100

    Added set -e to run_tests.sh and run_end_to_end_tests.sh

--- run_end_to_end_tests.sh
+++ run_end_to_end_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 ./run_end_to_end_tests_python2.sh
 ./run_end_to_end_tests_python3.sh
 
--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -e
+
 ./run_tests_python2.sh
 ./run_tests_python3.sh
+
